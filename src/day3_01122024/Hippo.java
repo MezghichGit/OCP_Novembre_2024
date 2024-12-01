9746b99f8cd53c2279d5709fa46b51c8cba0fe4b
Mohamed Amine MEZGHICH <git@mezghich.com>
2024-12-01 15:44:48 +0100

    day 3

---
+++ src/day3_01122024/Hippo.java
@@ -0,0 +1,5 @@
+package day3_01122024;
+
+public class Hippo {
+
+}
---
+++ src/day3_01122024/Main.java
@@ -0,0 +1,21 @@
+package day3_01122024;
+
+import java.util.ArrayList;
+
+public class Main {
+
+	public static void main(String[] args) {
+		ArrayList ls = new ArrayList();
+		System.out.println(new ArrayList());
+		System.out.println(ls); // []
+		System.out.println(ls.toString()); // []
+		System.out.println(new String[0]); // [Ljava.lang.String;@65cc892e}
+		/*
+		int i =10;
+	    short j= i;
+	     
+	    final  int k =10;
+	    short m=k;*/
+	}
+
+}
